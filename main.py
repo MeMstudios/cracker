ce2265557bced2665c400efba70b0807fafdc312
MeMstudios <michaelmurphystudios@gmail.com>
2024-01-04 14:03:28 -0500

    simplify

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from anyapp import AppUser, UserSystem
 from cracker import Cracker
 
+# Initilize the Cracker
+my_cracker = Cracker("uppercase", "symbols", "digits", max_len=13, tries=12)
+my_cracker.init_guess_generator()
 
-my_cracker = Cracker("uppercase", max_len=4, tries=10000000)
-my_cracker.set_guess_generator()
-
+# Initialize the User System
 my_system = UserSystem()
 test_pw = "teST"
 user = AppUser(id=1, name="makool", password=test_pw)
 my_system.add_user(user)
-user = my_system.get_user("makool")
-print(user.check_password(test_pw))
 
+print(f"Password check works: {user.check_password(test_pw)}")
 print(f"Password is: {my_cracker.guess(user.check_password)}")
