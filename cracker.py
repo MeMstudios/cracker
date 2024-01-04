19b3d6c580456a4bc0d6cf2824e8dd7d0cda15a4
MeMstudios <michaelmurphystudios@gmail.com>
2024-01-04 13:11:58 -0500

    refactor functions. add guess function

--- cracker.py
+++ cracker.py
@@ -5,10 +5,10 @@ from typing import Generator
 
 class Cracker:
     """
-    Include any of the options shown below.  Number of tries is required.  Also, must include max_len or known_len.
+    Include any of the options shown below.  Number of tries is required.  Also, must include max_len or fixed_len.
     Tries will be the number of string generations.
     When including max_len it's recommended to include min_len as well.
-    Including known_len will make the set of guesses a fixed length and ignore the other len parameters.
+    Including fixed_len will make the set of guesses a fixed length and ignore the other len parameters.
     Using no options produces only lowercase letters.  While other options include lowercase by default,
     Using the "digitsonly" option will only have digits, useful for numeric pins.
 
@@ -17,30 +17,31 @@ class Cracker:
 
     charset: str
     max_len: int
-    min_len: int
-    tries: int
-    fixed: bool = False
+    fixed_len: int = 0
+    min_len: int = 1
+    tries: int = 0
     prefix: str = ""
+    guess_generator: Generator
 
     def __init__(self, *options, **params) -> None:
-        self.tries = 0 if "tries" not in params else params["tries"]
-        self.prefix = "" if "prefix" not in params else params["prefix"]
+        if "tries" in params:
+            self.tries = params["tries"]
+        if "prefix" in params:
+            self.prefix = params["prefix"]
 
         if self.tries == 0:
-            raise "Please include tries=number_of_tries"
+            raise BaseException("Please include a number of tries=more_than_zero")
 
-        # Initialize the parameters
-        if "known_len" in params:
-            self.fixed = True
-            self.max_len = params["known_len"]
+        if "fixed_len" in params:
+            self.fixed_len = params["fixed_len"]
         elif "max_len" in params:
             self.max_len = params["max_len"]
             if "min_len" in params:
                 self.min_len = params["min_len"]
         else:
-            raise "Please include a known_len or a max_len!"
+            raise BaseException("Please include a fixed_len or a max_len")
 
-        # Build the charset
+        # Build the charset from the options
         charset = string.ascii_lowercase
         if "uppercase" in options:
             charset += string.ascii_uppercase
@@ -54,12 +55,21 @@ class Cracker:
         self.charset = charset
 
     def generate_string(self) -> str:
-        num_chars = (
-            self.max_len - len(self.prefix)
-            if self.fixed
-            else random.randrange(self.min_len, self.max_len + 1) - len(self.prefix)
-        )
+        if self.fixed_len:
+            num_chars = self.fixed_len - len(self.prefix)
+        else:
+            num_chars = random.randrange(self.min_len, self.max_len + 1) - len(self.prefix)
         return self.prefix + ("".join(random.choices(self.charset, k=num_chars)))
 
-    def generate_guesses(self) -> Generator[str, str, str]:
-        return (self.generate_string() for _ in range(self.tries))
+    def set_guess_generator(self):
+        self.guess_generator = (self.generate_string() for _ in range(self.tries))
+
+    def generate_passwords(self) -> list[str]:
+        return [self.generate_string() for _ in range(self.tries)]
+
+    def guess(self, check_password_function) -> str:
+        for g in self.guess_generator:
+            if check_password_function(g):
+                print("Cracked!")
+                return g
+        return "not cracked :("
